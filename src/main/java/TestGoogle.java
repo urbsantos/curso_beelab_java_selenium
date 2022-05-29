3ac945035453f262a4dbecd293041ec8f533160f
Urbano Santos <urbanolopes@gmail.com>
2022-05-28 21:22:50 -0300

    Removing not used imports

--- src/main/java/TestGoogle.java
+++ src/main/java/TestGoogle.java
@@ -2,7 +2,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
--- src/main/java/TesteFramesEJanelas.java
+++ src/main/java/TesteFramesEJanelas.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteFramesEJanelas {
